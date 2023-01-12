a189d73ab1f8d34b78e0e4a36b2875fa6adb9063
Sotatek-KhanhNguyen5 <khanh.nguyen5@sotatek.com>
2023-01-12 10:30:15 +0700

    Eslint and Husky config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
 })
 export class AppModule {}
+console.log('first');
