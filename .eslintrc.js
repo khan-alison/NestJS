8345b9dacfa91642741d86c0179eea6cbe75b2ed
Sotatek-KhanhNguyen5 <khanh.nguyen5@sotatek.com>
2023-01-12 09:36:54 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    // 'simple-import-sort/imports': 'warn',
+    // 'simple-import-sort/exports': 'warn',
+    // 'import/first': 'warn',
+    // 'import/newline-after-import': 'warn',
+    // 'import/no-duplicates': 'warn',
+  },
+};
