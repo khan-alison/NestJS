0dabeeddcc29fe5eab205ac6009568bbdfabefb5
Nguyễn Nam Khánh <khanhnn@MacBook-Pro-cua-Nguyen.local>
2023-01-30 20:28:03 +0700

    Feature create new task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { TasksModule } from './tasks/tasks.module';
+import * as taskService from './tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
+  providers: [taskService.TasksService],
 })
 export class AppModule {}
-console.log('first');
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { title } from 'process';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTask();
+  }
+
+  @Post()
+  createTask(@Body('title') title, @Body('description') description): Task {
+    return this.tasksService.createTask(title, description);
+  }
+}
